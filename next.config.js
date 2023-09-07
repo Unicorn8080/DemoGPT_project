d413b3f23e09d14e72b1f52276952d866bad6f3e
john199310 <johnwilliam199024@gmail.com>
2023-09-07 15:06:27 +0100

    feat: dashboard

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+  reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/landingpage",
+        permanent: false,
+      },
+    ];
+  },
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+    },
   },
-}
+};
